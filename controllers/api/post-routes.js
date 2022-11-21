593b9993f0c8275d7c05daa6568347c658ec018a
Daven <davensteen@gmail.com>
2022-11-21 03:40:07 -0600

    add if statements

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -87,14 +87,17 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-    Vote.create({
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-      })
-        .then(dbPostData => res.json(dbPostData))
-        .catch(err => res.json(err));
+  // make sure the session exists first
+  if (req.session) {
+    // pass session id along with all destructured properties on req.body
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  }
 });
-
 router.put('/:id', (req, res) => {
     Post.update(
       {
